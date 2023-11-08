40afb7c4201283af72a754c39b8bc0a8ccab3f33
aniruddha <ani@dctacademy.com>
2023-11-08 09:18:39 +0530

    home and blogs link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { Routes, Route, Link } from 'react-router-dom'
+import Home from './components/Home';
+import BlogsList from './components/BlogsList';
 function App() {
   return (
     <div>
       <h1>Blog App</h1>
+      <nav>
+        <Link to="/"> Home </Link> | 
+        <Link to="/blogs"> Blogs  </Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blogs" element={<BlogsList />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/BlogsList.js
@@ -0,0 +1,7 @@
+export default function BlogsList() {
+    return (
+        <div>
+            <h2>BlogsList Component</h2>
+        </div>
+    )
+}
